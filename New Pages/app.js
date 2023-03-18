6511b8493aac07fe36319cdd20be0ff2957982f7
RaeE454 <rwatkins535@gmail.com>
2023-03-18 10:14:56 -0600

    Only a few things left to work on

---
+++ New Pages/app.js
@@ -0,0 +1,9 @@
+
+function linkedInBtn(link){
+    location.href = "https://www.linkedin.com/in/raewatkins53535/";
+};
+
+// Rating Initialization
+$(document).ready(function() {
+    $('#rateMe1').mdbRate();
+  });
