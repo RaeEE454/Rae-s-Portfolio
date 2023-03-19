37211d9a388eee1d5b036cdb2ebecbfa8a0cada2
RaeE454 <rwatkins535@gmail.com>
2023-03-19 16:31:36 -0600

    Added Files

--- New Pages/app.js
+++ New Pages/app.js
@@ -3,7 +3,8 @@ function linkedInBtn(link){
     location.href = "https://www.linkedin.com/in/raewatkins53535/";
 };
 
-// Rating Initialization
-$(document).ready(function() {
-    $('#rateMe1').mdbRate();
-  });
+// // Rating Initialization
+// $(document).ready(function() {
+//     $('#rateMe1').mdbRate();
+//   });
+
